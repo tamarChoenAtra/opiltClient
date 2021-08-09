6492024bf52f0470ccf0eab3c4f4ce58bf4bf808
TamarCohenAtra <tamarcohen@leader.codes>
2021-08-09 14:56:59 +0300

    tamar 09 redux , routing , env ..

--- App.js
+++ App.js
@@ -6,14 +6,22 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Routes } from './src/routes/routes';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
+// import keys from './src/config/keys';
 
 function App() {
 
+  useEffect(() => {
+    // alert(keys.API_URL)
+  }, [])
 
   return (
-    <>
-    </>
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   );
 };
 
