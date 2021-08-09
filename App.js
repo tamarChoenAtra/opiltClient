3dee6d6c275bfb4216b34bdf5f650ff255a671a8
TamarCohenAtra <tamarcohen@leader.codes>
2021-08-09 11:39:36 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+function App() {
+
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default App;
